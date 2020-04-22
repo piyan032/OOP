394e7b671e6ec14e925395d7795fc6999e173c9a
Piyan Saputra <58876544+piyan032@users.noreply.github.com>
2020-04-22 09:11:09 +0700

    Soal
    Soal

---
+++ tugas.php
@@ -0,0 +1,32 @@
+<?php
+
+
+// TUGAS 1
+// Buat class SMK
+// punya property
+// public = nama, umur, alamat, kelas
+// private = jurusan
+// protected = angkatan
+
+// buat class turunan
+// 1. Siswa
+// 2. Siswi
+// muncul 3 data untuk siswa dan 3 data untuk siswi
+
+
+
+// TUGAS 2
+// buat abstract class yg menentukan Mobil BMW dan Mobil Ferrari itu siapa yg paling cepet
+
+
+
+// TUGAS 3
+// buat interface MobilKeluarga
+// mobil keluarga start di tangerang
+// cek Mobil keluarga ini kalo mau ke bandung, perlu bensin nya berapa
+
+
+
+// TUGAS 4
+// Buat class WebDeveloper
+// type hinting array, untuk melihat siapa aja programmer web di PT ABCD
